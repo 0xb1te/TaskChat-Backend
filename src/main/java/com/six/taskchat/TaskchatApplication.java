5fb49ce82005dac678333ca1a027f2119c737953
0xb1te <0xb1te0xb2te@gmail.com>
2024-09-07 17:52:13 +0200

    :feat: - Added files to base project

---
+++ src/main/java/com/six/taskchat/TaskchatApplication.java
@@ -0,0 +1,13 @@
+package com.six.taskchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/six/taskchat/TaskchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.six.taskchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
